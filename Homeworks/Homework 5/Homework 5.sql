c531a90e68e6b4af702368b7f560863e059ab961
Bozhidar Petkov <xidz01bog@gmail.com>
2025-11-21 23:17:46 +0200

    Added homework 5

---
+++ Homeworks/Homework 5/Homework 5.sql
@@ -0,0 +1,28 @@
+﻿begin tran
+set xact_abort on
+	update customers
+	set lname = 'Монтгомъри', address = 'гр. Чепеларе, ул. ЗДРАВЕЦ 11'
+	where customer_id = 103
+
+	insert into orders
+	values(908, GETDATE(), 103, 169, 'гр. Чепеларе, ул. ЗДРАВЕЦ 11')
+
+	update orders
+	set employee_id = 169
+	where customer_id = 103
+
+	select 103 as cust_id, 169 as emp_id, (select min(order_date) from orders where customer_id = 103) as custFirstOrder, (select min(order_date) from orders where employee_id = 169) as empFirstOrder
+
+	save tran SaveTran
+
+	delete from customers
+	where customer_id not in (select customer_id from orders)
+
+	update employees set manager_id = 169
+	where employee_id not in (select employee_id from orders)
+
+	rollback transaction SaveTran
+
+	commit tran
+	
+
